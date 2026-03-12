464ec0da1cd7ba7496766f4876471b6a4a031579
tech-kurukshetra <btech_events@svgu.ac.in>
2026-03-12 10:11:20 +0000

    add

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-7106208473-d6a17",
-  "appId": "1:883152144766:web:9fac05bf739526608c4b2f",
-  "apiKey": "AIzaSyCZg9TK0-kMeYjCvnK_UAfBzvfmDExZnkU",
-  "authDomain": "studio-7106208473-d6a17.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "883152144766"
-};
